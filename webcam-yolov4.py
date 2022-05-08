03d309620327bc50014d3147b5112964a5c36606
Jonathon Nguyen <nguyjd@gmail.com>
2022-05-08 01:23:01 -0400

    Added counting

--- webcam-yolov4.py
+++ webcam-yolov4.py
@@ -3,15 +3,15 @@ import time
 import numpy as np
 
 # Threshold for the model.
-CONFTHRESHOLD = 0.3
-NMSTHRESHOLD = 0.3
+CONFTHRESHOLD = 0.5
+NMSTHRESHOLD = 0.5
 
 # font and color of the text and the box 
 COLOR = (0, 255, 0)
 FONT = cv2.FONT_HERSHEY_PLAIN
 
 
-yolov4 = cv2.dnn.readNet('backup\yolov4_1000.weights', 'yolov4.cfg')
+yolov4 = cv2.dnn.readNet('Trained_model.weights', 'yolov4.cfg')
 yolov4.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
 yolov4.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
@@ -21,6 +21,7 @@ yolov4.setInputParams(size=(128, 128), scale=1/255, swapRB=True)
 cam = cv2.VideoCapture(0)
 
 labels = ['banana', 'apple', 'eggplant']
+produceCount = [0, 0, 0]
 
 while True:
 
@@ -40,6 +41,21 @@ while True:
             label = f"{labels[labelsIndex[index]]}: {scores[index]:.2f}"
             cv2.rectangle(frame, bound, COLOR, 1)
             cv2.putText(frame, label, (bound[0], bound[1]-10), FONT, 1, COLOR, 1)
+
+            produceCount[0] = 0
+            produceCount[1] = 0
+            produceCount[2] = 0
+
+            if labels[labelsIndex[index]] == 'banana':
+                produceCount[0] += 1
+            elif labels[labelsIndex[index]] == 'apple':
+                produceCount[1] += 1
+            elif labels[labelsIndex[index]] == 'eggplant':
+                produceCount[2] += 1
+                    
+        print(f"Banana Count: {produceCount[0]}")
+        print(f"Apple Count: {produceCount[1]}")
+        print(f"Eggplant Count: {produceCount[2]}")
 
         # Check for a escape press to quit the program
         if cv2.waitKey(1) == 27:
